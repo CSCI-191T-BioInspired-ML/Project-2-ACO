502160f4730964851718b6afa127239fb7480f5d
Sarah Kern <84293034+sarahekern1@users.noreply.github.com>
2022-04-13 20:04:06 -0700

    Update nnheuristic.py
    Changed to a class

--- nnheuristic.py
+++ nnheuristic.py
@@ -1,48 +1,73 @@
 #Nearest neighbor heuristic
+from matplotlib.image import composite_images
 import numpy as np
+from cities import Cities
+import math
+
+class NN(object):
+
+    def __init__(self, size: int, graph: Cities):
+        self.cities = graph
+        self.numCities = size
+        self.bestPath = []
+        self.bestCost = math.inf
+        self.unvisited = [i for i in range(self.numCities)]    #unvisited cities that can still be visited
 
 
-#Returns the city index and cost *note*: not currently using the cost, can remove from return statement
-#If we keep it that way 
-#Before storing the next minimum distance, it checks if this city is already visited.
-#If it has been visited, then it skips that one and doesnt update min
-def minEdge(numCities, cost_matrix, startIndex, visited):
-    minEdge = 10000000
-    edgeIndex = 0
-    for i in range(numCities):
-        curEdge = cost_matrix[startIndex][i]
-        if curEdge != 0:
-            if curEdge < minEdge and visited[i] == 0:
-                minEdge = curEdge
-                edgeIndex = i
+    def solveNN(self):
+        visited=np.zeros(self.numCities)
+        path = []
+        cost = 0
+        for i in range(self.numCities):
+            (curCost, curPath) = self.nearestNeighbor(i, self.cities, visited, path, cost)
+            if curCost < self.bestCost:
+                self.bestCost = curCost
+                self.bestPath = curPath
+            visited=np.zeros(self.numCities)
+            path = []
+            cost = 0
+        return (self.bestPath, self.bestCost)
+        # print("Cost: ", self.bestCost)
+        # print("Path: ", self.bestPath)
 
-    return (edgeIndex, minEdge)
+    def minEdge(self, cost_matrix, startIndex, visited):
+        minEdge = 1000000000
+        edgeIndex = 0
+        for i in range(self.numCities):
+            curEdge = cost_matrix[startIndex][i]
+            if curEdge != 0:
+               # try:
+                    #self.unvisited.index(i)
+                if curEdge < minEdge and visited[i]==0:
+                    minEdge = curEdge
+                    edgeIndex = i
+               # except ValueError: 
+                #    pass
 
-def nearestNeighbor(startIndex, cost_matrix, numCities, visited, path):
-    #Setting the starting city to visited. 
-    visited[startIndex] = 1
+        return (edgeIndex, minEdge)
 
-    #Will store the order of cities visited
-    path.append(startIndex)
+    def nearestNeighbor(self,curCity: int, cities: Cities, visited, path, totalD):
+        #Setting the starting city to visited. 
 
-    #complete is true if all cities have been visited
-    complete = np.all(visited == visited[0])
+        visited[curCity] = 1
+        #Will store the order of cities visited
+        path.append(curCity)
 
-    #checking if all cities have been visited
-    #if yes, append start city to the end and return the path
-    if complete:
-        path.append(path[0])
+        #complete is true if all cities have been visited
+        complete = np.all(visited == visited[0])
+        #checking if all cities have been visited
+        #if yes, append start city to the end and return the path
+        if complete:
+            path.append(path[0])
+            totalD += cities.cost_matrix[curCity][path[0]]
+            path2 = path
+            return (totalD, path2)
     
-    #Otherwise, find next min distance
-    #Update visited 
-    #call nearest neighbor with new starting city
-    else:
-        (minI, minCost) = minEdge(numCities, cost_matrix, startIndex, visited)
-        visited[minI] = 1
-        nearestNeighbor(minI, cost_matrix, numCities, visited, path)
-
-
-
-
-
-
+        #Otherwise, find next min distance
+        #Update visited 
+        #call nearest neighbor with new starting city
+        else:
+            (minI, minCost) = self.minEdge(cities.cost_matrix, curCity, visited)
+            totalD += minCost
+            visited[minI] = 1
+            return self.nearestNeighbor(minI, cities, visited, path, totalD)
