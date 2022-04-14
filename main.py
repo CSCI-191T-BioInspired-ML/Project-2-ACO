4552971fa06d230eeda18286686aad5996ff1d5d
Sarah Kern <84293034+sarahekern1@users.noreply.github.com>
2022-04-13 20:04:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,27 @@
-from plot import plot, plot2
-from nnheuristic import nearestNeighbor
-import math
-import numpy as np
+from cmath import inf
+from plot import plot, plot_points
+from nnheuristic import NN
+from aco import ACO
+from cities import Cities
 global numCities
 
-#Function to read the cities #, x, and y values from txt file. 
-#Stores city data in dictionary cities
-#Stores just x,y data in array points
-def getCities(points, cities, fileName):
-    with open(fileName) as f:
-        for line in f.readlines():
-            city = line.split(' ')
-            cities.append(dict(index=int(city[0]), x=int(city[1]), y=int(city[2])))
-            points.append((int(city[1]), int(city[2])))
-
-#Returns the distance between two cities. Will be stored in cost matrix
-def distance(city1: dict, city2: dict):
-    return math.sqrt((city1['x'] - city2['x']) ** 2 + (city1['y'] - city2['y']) ** 2)
-
-#Sets all the distances in the cost matrix.
-def setDistances(cost_matrix, cities):
-    for i in range(numCities):
-        for j in range(numCities):
-            cost_matrix[i][j] = distance(cities[i], cities[j])
-
-
-
 numCities = 50
-points = []
-cities = []
-cost_matrix=np.zeros([numCities,numCities])
+graph = Cities(50, 'data1.txt')
+plot_points(graph.points)
+antColony = ACO()
+nearestNeighbor= NN(numCities, graph)
 
-getCities(points, cities, 'data1.txt')
-setDistances(cost_matrix, cities)
+pathACO, costACO = antColony.solveACO(graph)
+pathNN, costNN = nearestNeighbor.solveNN()
 
-
-print("----------------Cities--------------------------")
-for i in range(numCities):   
-    print(cities[i])
-
-print()
-print()
-print("----------------Calling Nearest Neighbor--------------------------")
-visited=np.zeros(numCities)
-path=[]
-nearestNeighbor(47, cost_matrix, numCities, visited, path)
+print("ACO Cost: ", costACO)
+print("ACO path: ", pathACO)
 
 print()
 print()
-print("---------------Path--------------------------")
-print(path)
 
-#plot2(points)
-plot(points, path)
+print("NN Cost: ", costNN)
+print("NN path: ", pathNN)
+
+plot(graph.points, pathACO, costACO, 'ACO')
+plot(graph.points, pathNN, costNN, 'NN')
