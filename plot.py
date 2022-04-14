e06fd3dc311a617f11be8f90783cb03fbf9c7e30
Sarah Kern <84293034+sarahekern1@users.noreply.github.com>
2022-04-13 20:04:32 -0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,46 +1,36 @@
 import operator
 import matplotlib.pyplot as plt
-import math
-import numpy as np
 
 
-def plot(points, path):
+def plot(points, path, cost, type):
     x = []
     y = []
     for point in points:
         x.append(point[0])
         y.append(point[1])
-    # noinspection PyUnusedLocal
+    
     y = list(map(operator.sub, [max(y) for i in range(len(points))], y))
     plt.plot(x, y, 'co')
+    plt.figtext(0.5,0.95, ("Total Distance: ", cost), ha="center", va="center", fontsize=18, bbox={"facecolor":"r", "alpha":0.5})    
+    plt.xlabel('X', fontsize = 15)
+    plt.ylabel('Y', fontsize = 15)
 
     for _ in range(1, len(path)):
         i = path[_ - 1]
         j = path[_]
-        # noinspection PyUnresolvedReferences
         plt.arrow(x[i], y[i], x[j] - x[i], y[j] - y[i], color='r', length_includes_head=True)
-
-    # noinspection PyTypeChecker
-   # plt.xlim(0, max(x) * 1.1)
-    # noinspection PyTypeChecker
-    #plt.ylim(0, max(y) * 1.1)
     plt.show()
 
-def plot2(points):
+def plot_points(points):
     x = []
     y = []
     for point in points:
         x.append(point[0])
         y.append(point[1])
-    # noinspection PyUnusedLocal
-    # y = list(map(operator.sub, [max(y) for i in range(len(points))], y))
     plt.plot(x, y, 'co')
-    # noinspection PyTypeChecker
     plt.xlim(0, max(x) * 1.1)
-    # noinspection PyTypeChecker
     plt.ylim(0, max(y) * 1.1)
     plt.show()
-    print(x)
-    print(y)
+
 
 
