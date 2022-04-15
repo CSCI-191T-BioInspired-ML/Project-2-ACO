6ea98dca409e0d9e883190a93d0683ba091320d8
Sarah Kern <84293034+sarahekern1@users.noreply.github.com>
2022-04-14 17:33:52 -0700

    Update aco.py

--- aco.py
+++ aco.py
@@ -4,16 +4,27 @@ import random
 from cities import Cities
 
 class ACO(object):    
+    """
+    m:          # of ants
+    n:          # of cities
+    alpha:      pheromone factor, exploitation, increasing leads to too many options
+    beta:       visibility factor (weight of distances), exploratin, more can lead o slow or no convergence
+    rho:        evaporation rate
+    iterations: # of iterations to run
+    antPaths:   the paths of the ants. 1 more than # of cities to return to start city
+    Q:          constant for determining pheromone difference
+    
+    """
     #This is the default constructor
     def __init__(self):
-        self.m = 10        # of ants to search with
-        self.n = 50       #number of cities in graph
-        self.alpha = 1.2          #pheromone factor, exploitation, lead to too many options
-        self.beta = 1.7          #visibility fator, exploration, more can lead to slow or no convergence        
-        self.rho = 0.2         #evap rate
-        self.iterations = 100  #number of iterations
-        self.antPaths = np.zeros((self.m,self.n+1))     #one extra city to get back to start city.
-        self.q = 5   
+        self.m = 25        
+        self.n = 50      
+        self.alpha = 1.7          
+        self.beta = 1.7            
+        self.rho = 0.2         
+        self.iterations = 150  
+        self.antPaths = np.zeros((self.m,self.n+1))     
+        self.Q = 1          
 
     #Constructor to set parameters yourself
     # def __init__(self, n_ants: int, n_cities: int, iter: int, a: float, b: float, evap: float, Q: int):
@@ -27,11 +38,14 @@ class ACO(object):
     #     self.antPaths = np.zeros((self.m,self.n+1))     #one extra city to get back to start city.
     #     self.q = Q
 
+
+
     def updatePheromone(self, cities: Cities, ants: list):
         for i, row in enumerate(cities.pheromone):
             for j, col in enumerate(row):
                 cities.pheromone[i][j] *= self.rho
                 for ant in ants:
+                    ant.pd[i][j]
                     cities.pheromone[i][j] += ant.pd[i][j]
 
 
@@ -39,81 +53,96 @@ class ACO(object):
         minCost = inf
         bestSolution= []
         for iter in range(self.iterations):
-            ants = [Ant(cities.numCities, cities,self) for i in range(self.m)]
-            for ant in ants:
-                for i in range(cities.numCities-1):
+            ants = [Ant(cities,self) for i in range(self.m)]            # Creates array of ants
+            for ant in ants:            
+                for i in range(cities.numCities-1):                     # Choose next city for every ant
                     ant.chooseNext()
-                ant.pathCost += cities.cost_matrix[ant.path[-1]][ant.path[0]]
+                ant.pathCost += cities.cost_matrix[ant.path[-1]][ant.path[0]]   # Adds cost of last city back to start city
                 if ant.pathCost < minCost:
                     minCost = ant.pathCost
-                    bestSolution = [] + ant.path
+                    bestSolution = [] + ant.path 
                 ant.updatePD()
             self.updatePheromone(cities, ants)
         
-        bestSolution.append(bestSolution[0])
-        minCost += cities.cost_matrix[bestSolution[0]][bestSolution[cities.numCities-1]]
+        bestSolution.append(bestSolution[0])        # Appends start city onto the path 
         return bestSolution, minCost
 
 
 class Ant(object):
     """
-    Ant object. One of these for every single ant.
-    Stores the path, the visited, the start city, etc
+    Ant object. 
+
+    cities:         This is the graph
+    colony:         The ant colony ACO
+    size:           Number of cities
+    path:           The path order of the ant
+    pathCost:       How much the path costs up to this point
+    unvisited:      The cities that are left to visit
+    startCity:      The city the ant starts at, chosen randomly
+    pd:             Pheromone delta list.
+    current:        The current city being visited
+    dist:           This is the inverse (1/distance) of the distance
     """
 
-    def __init__(self, numCities: int, cities: Cities, aco: ACO):
+    def __init__(self, cities: Cities, aco: ACO):
         self.cities = cities
         self.colony = aco
-        self.size = numCities
-        self.path = []              #order of cities visited
-        self.pathCost = 0           #cost of cities visited
-        self.unvisited = [i for i in range(numCities)]    #unvisited cities that can still be visited
-        startCity = random.randint(0, numCities-1)  # Sets start city to a random city
-        self.pd = []                   #pheromone delta: local increase of pheromone value
-        self.path.append(startCity)                 # adds the start city to the path
-        self.current = startCity                    # sets the current city to the start city
-        self.unvisited.remove(startCity)              # removes start city from the allowed list
+        self.size = cities.numCities
+        self.path = []                                      
+        self.pathCost = 0                                   
+        self.unvisited = [i for i in range(self.size)]      
+        startCity = random.randint(0, self.size-1)          
+        self.pd = []                                        
+        self.path.append(startCity)                         # adds the start city to the path
+        self.current = startCity                           
+        self.unvisited.remove(startCity)                    # removes start city from the unvisited list
         self.dist = [[0 if i == j else 1/cities.cost_matrix[i][j] for j in range(cities.numCities)]
-                    for i in range (cities.numCities)]
+                    for i in range (cities.numCities)]      # sets distances to 1 of distance based on cost_matrix
 
-
+    # Choosing the next city by calculating thr probabilities
+    # Using the equation
     def chooseNext(self):
-        sum = 0
+
+        p = np.zeros(self.size)         # p is the set of probabilities to the unvisited cities
+        sumDenom = 0                    # The sum for the denominator 
+
+        # Calculating the sum for the denominator
         for i in self.unvisited:
-            sum += self.cities.pheromone[self.current][i] ** self.colony.alpha * self.dist[self.current][i] ** self.colony.beta
-        
-        # p is the set of probabilities to the allowed cities (unvisited)
-        p = [0 for i in range(self.cities.numCities)]
-        
+            sumDenom += ((self.cities.pheromone[self.current][i] ** self.colony.alpha) 
+                    * (self.dist[self.current][i] ** self.colony.beta))
+
+        # Determining probabilities from current city to all unvisited cities.        
         for i in range(self.size):
             try:
                 self.unvisited.index(i)
-                p[i] = (self.cities.pheromone[self.current][i] ** self.colony.alpha * 
-                    self.dist[self.current][i] ** self.colony.beta) / sum
+                numer = ((self.cities.pheromone[self.current][i] ** self.colony.alpha) 
+                    * (self.dist[self.current][i] ** self.colony.beta))
+                p[i] = numer/sumDenom
             except ValueError:
                 pass
 
-        #use probabilities to choose the next city
+        # Use probabilities to choose the next city
+        # What do you guys think of this random aspect? 
+        # Should it be changed???
         nextCity = 0
-        randNum = random.random()
-        for i, prob in enumerate(p):
+        randNum = random.random()     
+        for i, prob in enumerate(p):    
             randNum -= prob
-            if randNum <= 0:
+            if randNum <= 0:            
                 nextCity = i
                 break
         
-        self.unvisited.remove(nextCity)
-        self.path.append(nextCity)
-        self.pathCost += self.cities.cost_matrix[self.current][nextCity]
-        self.current = nextCity
+        self.unvisited.remove(nextCity)     # Removes next city from unvisited
+        self.path.append(nextCity)          # Adds next city to the ant's path
+        self.pathCost += self.cities.cost_matrix[self.current][nextCity]    # Updating path cost
+        self.current = nextCity             # Updating current city to the next city
 
-
-    #this updates all delta t(i,j)
+    # Updating all the pheromone deltas.
     def updatePD(self):
-        self.pd = [[0 for j in range(self.cities.numCities)] for i in range(self.cities.numCities)]
-        for _ in range(1, len(self.path)):
-            i = self.path[_ - 1]
-            j = self.path[_]
-            #this is contsant q/over current path cost
-            self.pd[i][j] = self.colony.q / self.pathCost
+        self.pd = np.zeros([self.size, self.size])          # Setting pd to 2d array of 0's
+        for k in range(1, len(self.path)):                  # Starting at 1 because we subtract 1 
+            i = self.path[k - 1]                            # Finding previous city index
+            j = self.path[k]                                # Finding next city index
 
+            # PD is contsant Q divided by current path cost
+            self.pd[i][j] = self.colony.Q / self.pathCost
